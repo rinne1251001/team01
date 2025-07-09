335ed30509bcd1038fed733fb9d391c5aa6ecec1
rinne1251001 <cfrm1251001@gn.iwasaki.ac.jp>
2025-07-10 08:38:37 +0900

    21

--- resources/views/collections/create.blade.php
+++ resources/views/collections/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-@include('commons.errors')
+
 <main>
-    <form action="{{ route('collections.store') }}" method="POST">
+    <form id="collectionForm" action="{{ route('collections.store') }}" method="POST">
         @csrf
 
         <div class="form-group">
@@ -16,11 +16,17 @@
         </div>
         <div class="form-group">
             <label for="name">なまえをつけてね</label>
-            <input type="text" name="name" class="form-design" value="{{ old('name') }}">
+            <input type="text" name="name" class="form-design @error('name') is-invalid @enderror" value="{{ old('name') }}">
+            @error('name')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="memo">メモ</label>
-            <textarea name="memo" class="form-design">{{ old('memo') }}</textarea>
+            <textarea name="memo" class="form-design @error('name') is-invalid @enderror">{{ old('memo') }}</textarea>
+            @error('memo')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
         </div>
         
         <div class="btn-group">
@@ -30,4 +36,19 @@
         </div>
     </form>
 </main>
+
+<script>
+    document.getElementById("collectionForm").addEventListener("submit", function () {
+        const name = document.getElementsByName("name")[0].value.trim();
+        const memo = document.getElementsByName("memo")[0].value.trim();
+
+        if (name === "" || memo === "") {
+            let msg = "";
+            if (name === "") msg += "なまえを入力してください。\n";
+            if (memo === "") msg += "メモを入力してください。";
+            alert(msg);
+        }
+    });
+</script>
+
 @endsection
--- resources/views/collections/edit.blade.php
+++ resources/views/collections/edit.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-@include('commons.errors')
+
 <main>
-    <form action="{{ route('collections.update', $collection->id) }}" method="POST">
+    <form id="collectionForm" action="{{ route('collections.update', $collection->id) }}" method="POST">
         @method('patch')
         @csrf
 
@@ -12,11 +12,17 @@
         </div>
         <div class="form-group">
             <label for="name">なまえをつけてね</label>
-            <input type="text" name="name" class="form-design" value="{{ old('name', $collection->name) }}">
+            <input type="text" name="name" class="form-design @error('name') is-invalid @enderror" value="{{ old('name', $collection->name) }}">
+            @error('name')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="memo">メモ</label>
-            <textarea name="memo" class="form-design">{{ old('memo', $collection->memo) }}</textarea>
+            <textarea name="memo" class="form-design @error('name') is-invalid @enderror">{{ old('memo', $collection->memo) }}</textarea>
+            @error('memo')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
         </div>
         
         <div class="btn-group">
@@ -26,4 +32,19 @@
         </div>
     </form>
 </main>
+
+<script>
+    document.getElementById("collectionForm").addEventListener("submit", function () {
+        const name = document.getElementsByName("name")[0].value.trim();
+        const memo = document.getElementsByName("memo")[0].value.trim();
+
+        if (name === "" || memo === "") {
+            let msg = "";
+            if (name === "") msg += "なまえを入力してください。\n";
+            if (memo === "") msg += "メモを入力してください。";
+            alert(msg);
+        }
+    });
+</script>
+
 @endsection
